12769dfd2157f3f062766aa8df6a7c2aaa0262d7
Hasina Akter <hasina2421@gmail.com>
2022-05-10 09:55:26 +0600

    complete blogs

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "./Blogs.css"
 
 const Blogs = () => {
     return (
-        <div>
-            <h1> questions part </h1>
+        <div className='question-container'>
+            <h1>Questions And Answers</h1>
+            <div className='question'>
+                <h3>1. Different Between Authentication and Authorization?</h3>
+                <p>Answer:<br/> <br/>1. Authentication: Authentication confirms the identity of the user. Passwords, one-time pins, biometric information, and other information provided or entered by the user are used for authentication. The first step in a successful identity and access management strategy is authentication. The user can see and alter several aspects of authentication.
+                    <br /> <br/> 2. Authorization: Authorization determines which resources a user is allowed to access.The organization implements and maintains the parameters that govern authorization.Authentication is always followed by authorization.The user has no visibility into or control over authorization.</p>
+
+            </div>
+            <div className='question'>
+                <h3>2. Why are you using firebase? What other options do you have to implement authentication?</h3>
+                <p>Answer: Firebase Authentication makes it simple to get your users signed in without having to understand the complexities of designing your own authentication solution. Before we can use Firebase Storage, we should first authenticate a user with Firebase authentication. Default security rules require users to be authenticated. Firebase Storage is a powerful and simple object storage that allows you to store your files with ease.</p>
+            </div>
+            <div className='question'>
+                <h3>3. What other services does firebase provide other than authentication</h3>
+                <p>Answer: Firebase is a comprehensive solution that may help you build mobile or web applications more quickly and efficiently while using fewer resources. Let's look at how some of the services are used. Authentication, hosting, cloud storage, Google Analytics, predictions, cloud messaging, dynamic links, and remote configuration are all Cloud Firestore Cloud Functions.</p>
+            </div>
         </div>
     );
 };
--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 
 
 import "./Login.css";
 
 const Login = () => {
+    const location = useLocation();
     const navigate = useNavigate();
+    const [signInWithGoogle] = useSignInWithGoogle(auth)
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
     const [
         signInWithEmailAndPassword,
         user,
@@ -17,25 +20,35 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
-    const handleEmailBlur = event =>{
+    const handleEmailBlur = event => {
         setEmail(event.target.value);
     }
 
-    const handlePasswordBlur =event =>{
+    const handlePasswordBlur = event => {
         setPassword(event.target.value);
     }
     const handleUserSignIn = event => {
         event.preventDefault();
         signInWithEmailAndPassword(email, password);
     }
-    if(user){
+
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate('/home')
+            })
+    }
+    if (user) {
         navigate('/home');
+    
     }
     return (
         <div className='form-container'>
             <div>
                 <h2 className='form-title'>Login</h2>
-                <form onSubmit={handleUserSignIn }>
+                <form onSubmit={handleUserSignIn}>
                     <div className="input-group">
                         <label htmlFor='email'>Email</label>
                         <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
@@ -45,7 +58,7 @@ const Login = () => {
                         <label htmlFor='password' >Password</label>
                         <input onBlur={handlePasswordBlur} type="password" name="password" id="" required />
                     </div>
-                 
+
                     <p style={{ color: 'red' }}>{error?.message}</p>
                     {
                         loading && <p>Loading...</p>
@@ -57,13 +70,14 @@ const Login = () => {
                     New to Ema-John? <Link className='form-link' to="/signup">Create an account</Link>
                 </p>
                 <div className='other-option'>
-                  <div>------------------------------</div>
-                  <h4>Or</h4>
-                  <div>------------------------------</div>
+                    <h4>Or</h4>
+                 </div>
+            
+                <div >                 
+                     <button onClick={handleGoogleSignIn} className='google-signin'>Continue With Google</button>
                 </div>
-                <button >Continue With Google</button>
             </div>
-          
+
 
         </div>
     );
